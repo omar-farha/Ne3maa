6a646d1c432d11f8fc95d46ceaf4db0694a02e98
omar-farha <farha.omar2008@gmail.com>
2025-03-15 20:13:53 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["yumppxlxfdyebewcptuf.supabase.co", "img.clerk.com"],
+  },
+};
+
+export default nextConfig;
